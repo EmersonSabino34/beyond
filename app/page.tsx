debfd0cdcc24526a7313408fb30c8d098c01e0d4
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2025-12-13 12:39:36 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { Footer } from "@/components/footer"
 // import { Store } from "@/components/footer"
 
 export default function Home() {
+
   return (
     <main className="min-h-screen">
       <Header />
