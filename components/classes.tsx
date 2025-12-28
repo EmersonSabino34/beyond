e07aff3d58a769c1244ccd0cc7b0e11f1e96096c
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2025-12-28 14:16:42 +0000

    Update classes.tsx

--- components/classes.tsx
+++ components/classes.tsx
@@ -4,7 +4,6 @@ import { MessageCircle } from "lucide-react"
 
 export function Classes() {
   const classes = [
-    
     {
       name: "Jiu-Jitsu",
       description: "Arte marcial brasileira focada em técnicas de solo e submissões",
