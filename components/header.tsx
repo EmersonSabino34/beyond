6351d4a5207b3a14c9227b64966bc8bdf19fd101
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-08-16 22:30:14 +0100

    Atualizar o header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react"
 import { Menu, X, Instagram } from "lucide-react"
 import Link from "next/link"
 
-
 export function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
