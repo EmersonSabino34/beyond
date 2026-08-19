cc2195fcd7d6f5938223d91e48071c282af550cd
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-08-19 22:37:57 +0100

    Atualizar o hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -35,6 +35,7 @@ export function Hero() {
             className="border-white text-white hover:bg-white hover:text-black text-lg px-8 bg-transparent"
             asChild
           >
+
             <a
               href="https://www.instagram.com/beyond_peniche"
               target="_blank"
