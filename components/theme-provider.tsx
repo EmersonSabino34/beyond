9aaa0d45cea322a946d9e0bd5d58b0a7a06793df
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-08-20 23:24:26 +0100

    Atualizar o theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -8,6 +8,7 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
